8c331ef57dd1dd4ad4d23b8c250e65587ce1f0e4
_Mark24_(张阳) <mark.zhangyoung@gmail.com>
2018-10-08 16:13:45 +0800

    refactor(rename): change name
    change name to FireAnt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import Formaker from "./robot";
+import Formaker from "./fireAnt";
 import Input from "./input";
 
 class App extends Component {
