90c9f5db294f1d3213e7a9acf0ee1c8d30dc3a2b
_Mark24_(张阳) <mark.zhangyoung@gmail.com>
2018-10-11 16:56:41 +0800

    feat(dynamic): dynamic test
    dynamic test

--- src/index.js
+++ src/index.js
@@ -1,106 +1,128 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import { Button } from "antd";
 import Formaker from "./fireAnt";
 import divComp from "./testContainerComp";
 import Input from "./testInputComp";
 import formProtocol from "./testFormProtocol";
 import TagPlugin from "./tagPlugin";
+import { cloneDeep } from "lodash";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.formaker = new Formaker(this.formConfig);
-    this.formaker.use(Input);
-    this.formaker.use(divComp);
-    this.formaker.use(formProtocol);
-    this.formaker.use(TagPlugin);
-  }
-  formConfig = {
-    version: "0.0.1",
-    app: {
-      formFields: [
-        {
-          name: "test",
-          type: "button",
-          props: {
-            type: "danger"
-          },
-          methods: {
-            onClick: () => {
-              console.log("hello");
+
+    this.state = {
+      formConfig: {
+        version: "0.0.1",
+        app: {
+          formFields: [
+            {
+              name: "test",
+              type: "button",
+              props: {
+                type: "danger"
+              },
+              methods: {
+                onClick: () => {
+                  console.log("hello");
+                }
+              },
+              child: "测试",
+              extends: null,
+              nodeHandler: "default"
+            },
+            {
+              name: "test2",
+              type: "button",
+              props: {
+                type: "danger"
+              },
+              methods: {
+                type: "primary"
+              },
+              child: "测试2222",
+              extends: null,
+              nodeHandler: "default"
+            },
+            {
+              name: "test3",
+              type: "input",
+              extends: null,
+              nodeHandler: "default"
+            },
+            {
+              name: "test4",
+              type: "input",
+              props: {
+                placeholder: "teAAAA"
+              },
+              extends: null,
+              nodeHandler: "default"
             }
-          },
-          child: "测试",
-          extends: null,
-          nodeHandler: "default"
-        },
+          ]
+        }
+      }
+    };
+  }
+  changeConf = () => {
+    const addition = {
+      name: "test5",
+      type: "divComp",
+      props: {
+        placeholder: "teAAAA"
+      },
+      extends: null,
+      nodeHandler: "default",
+      // children: "test",
+      child: [
         {
-          name: "test2",
-          type: "button",
-          props: {
-            type: "danger"
-          },
-          methods: {
-            type: "primary"
-          },
-          child: "测试2222",
-          extends: null,
-          nodeHandler: "default"
-        },
-        {
-          name: "test3",
-          type: "input",
-          extends: null,
-          nodeHandler: "default"
-        },
-        {
-          name: "test4",
-          type: "input",
-          props: {
-            placeholder: "teAAAA"
-          },
-          extends: null,
-          nodeHandler: "default"
-        },
-        {
-          name: "test5",
+          name: "test6",
           type: "divComp",
           props: {
-            placeholder: "teAAAA"
+            placeholder: "testChild"
           },
           extends: null,
           nodeHandler: "default",
-          // children: "test",
           child: [
             {
-              name: "test6",
-              type: "divComp",
+              name: "test7",
+              type: "tag",
               props: {
-                placeholder: "testChild"
+                color: "magenta"
               },
-              extends: null,
-              nodeHandler: "default",
-              child: [
-                {
-                  name: "test7",
-                  type: "tag",
-                  props: {
-                    color: "magenta"
-                  },
-                  child: "ssss"
-                }
-              ]
+              child: "ssss"
             }
           ]
         }
       ]
-    }
+    };
+
+    const formConfig = cloneDeep(this.state.formConfig);
+    console.log(formConfig);
+    formConfig.app.formFields.push(addition);
+    console.log("----");
+    console.log(formConfig);
+
+    this.setState({
+      formConfig
+    });
+  };
+
+  formaker = () => {
+    const formaker = new Formaker(this.state.formConfig);
+    formaker.use(Input);
+    formaker.use(divComp);
+    formaker.use(formProtocol);
+    formaker.use(TagPlugin);
+    return formaker.render();
   };
   render() {
     return (
       <div className="App">
         <p>生成组件</p>
-        <div>{this.formaker.render()}</div>
+        <Button onClick={this.changeConf}>动态改变</Button>
+        <div>{this.formaker()}</div>
       </div>
     );
   }
